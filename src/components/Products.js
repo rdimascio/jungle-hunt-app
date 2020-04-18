05d4a1483112c6a7da4f1df1e177af2d002714b1
Ryan DiMascio <ryan.dimascio@gmail.com>
2020-04-18 01:48:56 -0700

    Initial commit :zap:

---
+++ src/components/Products.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import styled from 'styled-components'
+import { useQuery } from '@apollo/react-hooks'
+
+const PRODUCTS = ({collection, limit, offset}) => gql`
+    {
+        Products(collection: "${collection}", limit: ${limit}, offset: ${offset}) {
+            asin,
+            title,
+            category {
+                primary,
+                secondary
+            },
+            image,
+            price,
+            rating,
+            reviews,
+            rank,
+        }
+    }
+`
+
+const ProductWrapper = styled.div`
+    width: 30%
+`
+
+const ProductImage = styled.div`
+    padding-bottom: 100%;
+    background-position: center;
+    background-size: contain;
+    background-image: url('${props => props.background}');
+`
+
+const Products = (props) => {
+    const { loading, error, data } = useQuery(PRODUCTS({collection: props.listType || 'bestSeller', limit: 24, offset: 0}), {
+        // variables: { collection: listType || 'bestSeller' }
+    })
+
+    if (loading) return 'Loading...'
+    if (error) return `Error! ${error}`
+
+    return data.Products.map((product) => {
+        /* eslint-disable no-unused-expressions */
+        return <ProductWrapper key={product.asin}>
+            <ProductImage background={product.image}></ProductImage>
+            <p>
+                {product.title}
+            </p>
+        </ProductWrapper>
+    })
+}
+
+export default Products
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import styled from 'styled-components'
+import ApolloClient from 'apollo-boost'
+import Products from './components/Products'
+import { ApolloProvider } from '@apollo/react-hooks'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const TOKEN = process.env.REACT_APP_JUNGLE_HUNT_API_TOKEN || ''
+const client = new ApolloClient({
+    uri: 'http://api.junglehunt.io',
+    headers: {
+        authorization: TOKEN ? `Bearer ${TOKEN}` : ''
+    },
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const ProductGrid = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`
+
+const App = () => {
+    /* eslint-disable no-unused-expressions */
+    return <ApolloProvider client={client}>
+        <ProductGrid>
+            <Products listType='newRelease' />
+        </ProductGrid>
+    </ApolloProvider>
+}
+
+render(<App />, document.getElementById('root'))
